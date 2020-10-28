e896ba208c4c88ae81cedfd47e2ab11799564bd4
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-10-27 21:50:15 -0300

    Update Exercícios Python #006 - Dobro, Triplo, Raiz Quadrada.py

---
+++ CURSO EM VIDEO/Exercícios Python #006 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,9 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro
+# triplo e raiz quadrada.
+valor = int(input('Digite um número: '))
+dobro = valor * 2 
+triplo = valor * 3
+raizquadrada = valor ** (1/2)
+print('O dobro de {} vale {}. '.format(valor, dobro ))
+print('O triplo de {} vale {}.'.format(valor, triplo))
+print('A raiz quadrada de {} vale {}.'.format(valor, raizquadrada)) 
