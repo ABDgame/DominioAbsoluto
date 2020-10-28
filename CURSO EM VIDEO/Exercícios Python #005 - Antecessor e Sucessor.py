01587744ad23a155557f049d58e109c520a3d11b
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-10-27 21:48:37 -0300

    Update Exercícios Python #005 - Antecessor e Sucessor.py

---
+++ CURSO EM VIDEO/Exercícios Python #005 - Antecessor e Sucessor.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu
+# sucessor e seu antecessor.
+valor = int(input('Digite um número: ')) 
+print('Analisando o número {}, seu antecessor é {} e o seu sucessor é {}'.format(valor,(valor-1),(valor+2))) 
