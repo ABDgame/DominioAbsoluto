0009d25bda31b4afd93c9e6ce7a309ccff3bb001
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-25 22:25:37 -0300

    Update Exercícios Python #062  Exercicio 061 melhorado.py

--- CURSO EM VIDEO/Exercícios Python #062  Exercicio 061 melhorado.py
+++ CURSO EM VIDEO/Exercícios Python #062  Exercicio 061 melhorado.py
@@ -1,3 +1,27 @@
-# Desafio 062 - Melhore o DESAFIO 061, perguntando para o
-# usuário se ela quer mostrar mais alguns termos. O programa 
-# encerra quando ele disser que quer mostrar O termos.
+'''
+    Melhore o DESAFIO 052, perguntando 
+para o usuário se ele quer mostrar 
+mais alguns termos. O programa
+encerrará quando ele disser que quer
+mostrar 0 termos.
+
+'''
+print('○●' * 20)
+print('Gerador de PA')
+print('●○' * 20)
+primeiro = int(input('Primeiro termo: '))  
+razão = int(input('Razão da PA: '))
+termo = primeiro 
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+   total = total + mais
+   while cont <= total:
+       print('{} -> '.format(termo), end='')
+       termo += razão 
+       cont += 1
+   print('PAUSA')
+   mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finaliza com {} termos mostrados.'.format(total))
+   
