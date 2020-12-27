101f5e3ad52cb7e09ba99a17ecd32d1de9839803
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:35:26 -0300

    Delete Exercícios Python #014 -  Conversor de Temperaturas.py

--- CURSO EM VIDEO/Exercícios Python #014 -  Conversor de Temperaturas.py
+++
@@ -1,4 +0,0 @@
-# Desafio 014 - 
-Celsius = float(input('Informe a temperatura em Celsius: '))
-Fahrenheit = ((9*Celsius)/5) + 32
-print ( 'A temperatura de {} Celsius corresponde a {} Fahrenheit'.format(Celsius, Fahrenheit))
