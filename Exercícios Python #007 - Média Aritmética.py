0614fad01a2e8829aa36a2454ac9ae1ad65842b5
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:50:43 -0300

    Delete Exercícios Python #007 - Média Aritmética.py

--- Exercícios Python #007 - Média Aritmética.py
+++
@@ -1,7 +0,0 @@
-# Desafio 007 - 
-n1 = float(input ('Digite a primeira nota '))
-n2 = float(input('Digite a segunda nota '))
-n3 = float(input ('Digite a terceira nota '))
-n4 = float(input ('Digite a quarta nota '))
-mediabi = (n1+n2+n3+n4)/4
-print ('A media do aluno é:{:.1f} '.format(mediabi))
