87dcffa1d4bda36847a4f8d5633ade85871761be
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:35:02 -0300

    Delete Exercícios Python #012 - Calculando Descontos.py

--- CURSO EM VIDEO/Exercícios Python #012 - Calculando Descontos.py
+++
@@ -1,4 +0,0 @@
-# Desafio 012
-preço = float(input('Quanto é o preço do seu produto? R$'))
-novo = preço - (preço * 5/ 100)
-print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preço, novo))
