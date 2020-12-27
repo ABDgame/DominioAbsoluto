884b3cbf4b266b643c9d4957bbcad01580c3874c
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:45:11 -0300

    Delete Exercícios Python #051 - Progressão Aritmética .py

--- CURSO EM VIDEO/Exercícios Python #051 - Progressão Aritmética .py
+++
@@ -1,8 +0,0 @@
-# Desafio 051 - Desenvolva um programa que leia o primeiro termo e a razão de 
-# uma PA. No final, mostre os 10 primeiros termos dessa progressão.
-primeiro = int(input(' Primeiro termo: '))
-razão = int(input(' Razão: '))
-décimo = primeiro + (10 - 1) * razão 
-for contador in range(primeiro, décimo + razão, razão):
-    print ('{}'.format(contador), end=' -> ')
-print('ACABOU')
