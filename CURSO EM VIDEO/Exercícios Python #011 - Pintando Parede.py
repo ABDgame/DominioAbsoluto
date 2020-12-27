a829dc5323784c983c4cb780bab0283ffbfcaf9f
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:34:49 -0300

    Delete Exercícios Python #011 - Pintando Parede.py

--- CURSO EM VIDEO/Exercícios Python #011 - Pintando Parede.py
+++
@@ -1,7 +0,0 @@
-# Desafio 011
-largura = float(input('Largura da Parede '))
-altura = float(input (' Altura da Parede '))
-área = largura * altura
-print('Sua parede tem a dimensão de {}x{} e sua área é de{}m2'.format(largura, altura, área))
-tinta = área / 2
-print ('Para pintar essa parede, você precisará de {}L de tinta'.format(tinta))
