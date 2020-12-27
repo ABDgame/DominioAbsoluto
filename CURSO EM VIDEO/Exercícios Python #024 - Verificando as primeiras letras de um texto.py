cf68ef0f243564ff3c8be3e4f2406a8ddf2f7f77
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:37:57 -0300

    Delete Exercícios Python #024 - Verificando as primeiras letras de um texto.py

--- CURSO EM VIDEO/Exercícios Python #024 - Verificando as primeiras letras de um texto.py
+++
@@ -1,4 +0,0 @@
-# Crie um programa que leia o nome de uma cidade e diga se ela
-# começa ou não com o nome "CURITIBA".
-cidade = str(input('Em que cidade você nasceu?')).strip()
-print(cidade[:8].upper()== 'CURITIBA')
