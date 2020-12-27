0e56c59945aa62397965c0b8f797c0f83fd4fba3
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:35:42 -0300

    Delete Exercícios Python #015 - Aluguel de Carros.py

--- CURSO EM VIDEO/Exercícios Python #015 - Aluguel de Carros.py
+++
@@ -1,5 +0,0 @@
-# Desafio 015 - 
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos quilômetros rodados? '))
-pago = (dias * 60) + (km * 0.15)
-print('O total a pagar é de R${:.2f}'.format(pago))
