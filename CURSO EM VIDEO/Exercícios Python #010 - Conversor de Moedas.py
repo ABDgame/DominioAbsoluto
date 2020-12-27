66f1c3c74e0c07ffe4d9fffad31a492aff44fa1b
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:34:37 -0300

    Delete Exercícios Python #010 - Conversor de Moedas.py

--- CURSO EM VIDEO/Exercícios Python #010 - Conversor de Moedas.py
+++
@@ -1,5 +0,0 @@
-# Desafio 010 - Conversor de moedas
-real = float(input('Quanto de dinheiro, você tem na carteira? R$'))
-dolar = real / 4.31
-euro = real / 4.81
-print ('Com R${:.2f} você pode comprar U$${:.2f} e €{:.2f}'.format(real, dolar, euro ))
