e8b6fa36ce4b72f7a7564593334ca60e85b8d9f6
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:35:15 -0300

    Delete Exercícios Python #013 - Reajuste Salarial.py

--- CURSO EM VIDEO/Exercícios Python #013 - Reajuste Salarial.py
+++
@@ -1,4 +0,0 @@
-# Desafio 013 -
-salário = float(input('Qual é o salário  atual do funcionário? R$'))
-aumento = salário + (salário * 37/ 100)
-print(' O salário que era R${}, com o aumento  de  37% será  R${}'.format(salário,aumento))
