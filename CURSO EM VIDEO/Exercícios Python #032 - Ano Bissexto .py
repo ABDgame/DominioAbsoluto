a4bb80c17a00431c037d5b8055b83d33d4680401
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2020-12-27 16:39:45 -0300

    Delete Exercícios Python #032 - Ano Bissexto .py

--- CURSO EM VIDEO/Exercícios Python #032 - Ano Bissexto .py
+++
@@ -1,9 +0,0 @@
-# Exercício Python 032 - Ano Bissexto
-from datetime import date
-ano = int(input('Qual ano você quer analisar? '))
-if ano == 0:
-    ano = date.today().year
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-     print('O ano {} é Bissexto '.format(ano))
-else:
-     print ('O ano {} Não é Bissexto '.format(ano))
