33d6ee4a1ed3e6229862d303c8d723a47050f582
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-01-21 21:08:59 -0300

    Update Desafio #093 - Cadastro De Jogador De Futebol.py

--- CURSO EM VÍDEO/Desafio #093 - Cadastro De Jogador De Futebol.py
+++ CURSO EM VÍDEO/Desafio #093 - Cadastro De Jogador De Futebol.py
@@ -5,27 +5,6 @@ partidas ele jogou. Depois vai ler a quantidade de gols feitos em
 cada partida. No final, tudo isso será guardado em um dicionário,
 incluindo o total de gols feitos durante o campeonato.
        
-         
-
-aluno = dict()
-while True:
-    aluno['nome'] = str(input('Nome: '))
-    aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
-    if aluno['média'] >= 7:
-        aluno['situação'] = 'APROVADO'
-    elif 5 <= aluno['média'] < 7:
-        aluno['situação'] = 'RECUPERAÇÃO'
-    else:
-        aluno['situação'] = 'REPROVADO'
-    for chave, valor in aluno.items( ):
-        print(f'  - {chave} é igual a {valor}')
-    resposta = str(input('Quer Avaliar Outro Aluno? [S/N]'))
-    if resposta in 'Nn':
-        break
-print('-='*20)
-print('       Pausa Para O Café ')
-print('-='*20)
-
 '''
 jogador = dict()
 partidas = list()
